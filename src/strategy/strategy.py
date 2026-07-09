df189696229f1351b2f22d5dbee66b4503589f7e
piotr-opiola-softkraft <p.opiola@softkraft.co>
2026-07-09 19:37:58 +0200

    agent: implement rule rule_05_fft_cycle_v6 (signal)

--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -15,6 +15,7 @@ import logging
 from types import ModuleType
 
 import src.strategy.rules.rule_04_arima_forecast.v2 as rule_04_arima_forecast_v2
+import src.strategy.rules.rule_05_fft_cycle.v6 as rule_05_fft_cycle_v6
 import src.strategy.rules.rule_05_fft_cycle.v5 as rule_05_fft_cycle_v5
 import src.strategy.rules.rule_24_adx_filtered_moving_average_cr.v1 as rule_24_adx_filtered_moving_average_cr_v1
 import src.strategy.rules.rule_22_triple_moving_average_crossove.v2 as rule_22_triple_moving_average_crossove_v2
@@ -41,6 +42,7 @@ ACTIVE_RULES: list[ModuleType] = [
     rule_22_triple_moving_average_crossove_v2,
     rule_24_adx_filtered_moving_average_cr_v1,
     rule_05_fft_cycle_v5,
+    rule_05_fft_cycle_v6,
 ]
 
 
