c06a181fc2334b6961c0f85b660f53f147bb3948
piotr-opiola-softkraft <p.opiola@softkraft.co>
2026-07-11 20:22:09 +0200

    revert

--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -15,29 +15,11 @@ import logging
 from types import ModuleType
 
 import src.strategy.rules.rule_02_bollinger_band.v1 as rule_02_bollinger_band_v1
-import src.strategy.rules.rule_24_bollinger_band_breach_with_mfi.v1 as rule_24_bollinger_band_breach_with_mfi_v1
-import src.strategy.rules.rule_23_bollinger_band_breach_with_eng.v1 as rule_23_bollinger_band_breach_with_eng_v1
-import src.strategy.rules.rule_02_bollinger_band.v8 as rule_02_bollinger_band_v8
-import src.strategy.rules.rule_19_bollinger_band_with_volume_spi.v3 as rule_19_bollinger_band_with_volume_spi_v3
-import src.strategy.rules.rule_22_bollinger_band_re_entry_with_v.v1 as rule_22_bollinger_band_re_entry_with_v_v1
-import src.strategy.rules.rule_21_bollinger_band_breakout_with_a.v1 as rule_21_bollinger_band_breakout_with_a_v1
-import src.strategy.rules.rule_15_vwap_bands_trading_rule.v3 as rule_15_vwap_bands_trading_rule_v3
-import src.strategy.rules.rule_02_bollinger_band.v7 as rule_02_bollinger_band_v7
-import src.strategy.rules.rule_19_bollinger_band_with_volume_spi.v2 as rule_19_bollinger_band_with_volume_spi_v2
-import src.strategy.rules.rule_18_average_directional_index_adx_.v2 as rule_18_average_directional_index_adx__v2
-import src.strategy.rules.rule_02_bollinger_band.v6 as rule_02_bollinger_band_v6
-import src.strategy.rules.rule_20_keltner_channel_mean_reversion.v2 as rule_20_keltner_channel_mean_reversion_v2
-import src.strategy.rules.rule_20_keltner_channel_mean_reversion.v1 as rule_20_keltner_channel_mean_reversion_v1
-import src.strategy.rules.rule_19_bollinger_band_with_volume_spi.v1 as rule_19_bollinger_band_with_volume_spi_v1
-import src.strategy.rules.rule_02_bollinger_band.v4 as rule_02_bollinger_band_v4
-import src.strategy.rules.rule_17_money_flow_index_mfi_overbough.v2 as rule_17_money_flow_index_mfi_overbough_v2
-import src.strategy.rules.rule_02_bollinger_band.v3 as rule_02_bollinger_band_v3
-import src.strategy.rules.rule_18_average_directional_index_adx_.v1 as rule_18_average_directional_index_adx__v1
-import src.strategy.rules.rule_17_money_flow_index_mfi_overbough.v1 as rule_17_money_flow_index_mfi_overbough_v1
-import src.strategy.rules.rule_15_vwap_bands_trading_rule.v2 as rule_15_vwap_bands_trading_rule_v2
-import src.strategy.rules.rule_15_vwap_bands_trading_rule.v1 as rule_15_vwap_bands_trading_rule_v1
-import src.strategy.rules.rule_02_bollinger_band.v2 as rule_02_bollinger_band_v2
 import src.strategy.rules.rule_04_arima_forecast.v1 as rule_04_arima_forecast_v1
+import src.strategy.rules.rule_19_bollinger_band_with_volume_spi.v1 as rule_19_bollinger_band_with_volume_spi_v1
+import src.strategy.rules.rule_19_bollinger_band_with_volume_spi.v2 as rule_19_bollinger_band_with_volume_spi_v2
+import src.strategy.rules.rule_19_bollinger_band_with_volume_spi.v3 as rule_19_bollinger_band_with_volume_spi_v3
+import src.strategy.rules.rule_24_bollinger_band_breach_with_mfi.v1 as rule_24_bollinger_band_breach_with_mfi_v1
 from src.agent.models import BuySignal, MarketData, SellSignal
 
 Signal = BuySignal | SellSignal
@@ -45,27 +27,9 @@ Signal = BuySignal | SellSignal
 ACTIVE_RULES: list[ModuleType] = [
     rule_02_bollinger_band_v1,
     rule_04_arima_forecast_v1,
-    rule_02_bollinger_band_v2,
-    rule_15_vwap_bands_trading_rule_v1,
-    rule_15_vwap_bands_trading_rule_v2,
-    rule_17_money_flow_index_mfi_overbough_v1,
-    rule_18_average_directional_index_adx__v1,
-    rule_02_bollinger_band_v3,
-    rule_17_money_flow_index_mfi_overbough_v2,
-    rule_02_bollinger_band_v4,
     rule_19_bollinger_band_with_volume_spi_v1,
-    rule_20_keltner_channel_mean_reversion_v1,
-    rule_20_keltner_channel_mean_reversion_v2,
-    rule_02_bollinger_band_v6,
-    rule_18_average_directional_index_adx__v2,
     rule_19_bollinger_band_with_volume_spi_v2,
-    rule_02_bollinger_band_v7,
-    rule_15_vwap_bands_trading_rule_v3,
-    rule_21_bollinger_band_breakout_with_a_v1,
-    rule_22_bollinger_band_re_entry_with_v_v1,
     rule_19_bollinger_band_with_volume_spi_v3,
-    rule_02_bollinger_band_v8,
-    rule_23_bollinger_band_breach_with_eng_v1,
     rule_24_bollinger_band_breach_with_mfi_v1,
 ]
 
