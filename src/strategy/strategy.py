338e0f6b92c24d79f0f6438d7989603bbc38f971
piotr-opiola-softkraft <p.opiola@softkraft.co>
2026-07-11 10:05:08 +0200

    just bollinger and arima

--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -15,14 +15,14 @@ import logging
 from types import ModuleType
 
 import src.strategy.rules.rule_02_bollinger_band.v1 as rule_02_bollinger_band_v1
-import src.strategy.rules.rule_04_arima_forecast.v2 as rule_04_arima_forecast_v2
+import src.strategy.rules.rule_04_arima_forecast.v1 as rule_04_arima_forecast_v1
 from src.agent.models import BuySignal, MarketData, SellSignal
 
 Signal = BuySignal | SellSignal
 
 ACTIVE_RULES: list[ModuleType] = [
     rule_02_bollinger_band_v1,
-    rule_04_arima_forecast_v2,
+    rule_04_arima_forecast_v1,
 ]
 
 
