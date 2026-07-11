aa01b06780102aecdfd0ed4f431d99d3ce6099ed
piotr-opiola-softkraft <p.opiola@softkraft.co>
2026-07-11 11:39:26 +0200

    revert

--- src/strategy/strategy.py
+++ src/strategy/strategy.py
@@ -15,14 +15,6 @@ import logging
 from types import ModuleType
 
 import src.strategy.rules.rule_02_bollinger_band.v1 as rule_02_bollinger_band_v1
-import src.strategy.rules.rule_16_volume_weighted_ma_crossover_w.v1 as rule_16_volume_weighted_ma_crossover_w_v1
-import src.strategy.rules.rule_15_adx_filtered_moving_average_cr.v1 as rule_15_adx_filtered_moving_average_cr_v1
-import src.strategy.rules.rule_13_macd_crossover_trend_following.v2 as rule_13_macd_crossover_trend_following_v2
-import src.strategy.rules.rule_04_arima_forecast.v4 as rule_04_arima_forecast_v4
-import src.strategy.rules.rule_04_arima_forecast.v3 as rule_04_arima_forecast_v3
-import src.strategy.rules.rule_02_bollinger_band.v2 as rule_02_bollinger_band_v2
-import src.strategy.rules.rule_14_ema_crossover_trend_following.v1 as rule_14_ema_crossover_trend_following_v1
-import src.strategy.rules.rule_04_arima_forecast.v2 as rule_04_arima_forecast_v2
 import src.strategy.rules.rule_04_arima_forecast.v1 as rule_04_arima_forecast_v1
 from src.agent.models import BuySignal, MarketData, SellSignal
 
@@ -31,14 +23,6 @@ Signal = BuySignal | SellSignal
 ACTIVE_RULES: list[ModuleType] = [
     rule_02_bollinger_band_v1,
     rule_04_arima_forecast_v1,
-    rule_04_arima_forecast_v2,
-    rule_14_ema_crossover_trend_following_v1,
-    rule_02_bollinger_band_v2,
-    rule_04_arima_forecast_v3,
-    rule_04_arima_forecast_v4,
-    rule_13_macd_crossover_trend_following_v2,
-    rule_15_adx_filtered_moving_average_cr_v1,
-    rule_16_volume_weighted_ma_crossover_w_v1,
 ]
 
 
